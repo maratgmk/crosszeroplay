fc98c910f0e1e3f87198daec3e0aea895b9e3295
Marat Gafiev <gmkorg109@gmail.com>
2024-06-02 10:25:08 +0300

    init play code was taken from GPT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,11 @@ import static org.example.Play.*;
 
 public class Main {
     public static void main(String[] args) {
+        
         initializeBoard();
         printBoard();
         Scanner scanner = new Scanner(System.in);
         char currentPlayer = PLAYER1;
+
     }
 }
